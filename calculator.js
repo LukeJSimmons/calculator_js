40c3cb3d7381a7a59383ea748bdbae0b7d032730
LukeJSimmons <lucasjsimmons@yahoo.com>
2025-03-12 09:06:35 -0500

    Add basic functions

--- calculator.js
+++ calculator.js
@@ -1,5 +1,36 @@
 class Calculator {
+    operate(operator, numbers) {
+        switch(operator) {
+            case '+':
+                return this.add(numbers)
+            case '-':
+                return this.subtract(numbers)
+            case '*':
+                return this.multiply(numbers)
+            case '/':
+                return this.divide(numbers)
+            default:
+                return
+        }
+    }
 
+    add(numbers) {
+        return numbers.reduce((acc, current) => acc + current, 0)
+    }
+
+    subtract(numbers) {
+        return numbers.reduce((acc, current) => acc - current)
+    }
+
+    multiply(numbers) {
+        return numbers.reduce((acc, current) => acc * current, 1)
+    }
+
+    divide(numbers) {
+        return numbers.reduce((acc, current) => acc / current)
+    }
 }
 
-console.log("Hello world")
+const calc = new Calculator
+
+console.log(calc.operate('+', [2,2]))
