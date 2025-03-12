0636e8631d27e656fa6b284eb41ebe1e80e9b1cd
LukeJSimmons <lucasjsimmons@yahoo.com>
2025-03-12 09:29:59 -0500

    Add operating on two numbers

--- calculator.js
+++ calculator.js
@@ -1,5 +1,7 @@
 class Calculator {
-    operate(operator, numbers) {
+    operate(input) {
+        const [operator, numbers] = this.getOperatorFromInput(input)
+
         switch(operator) {
             case '+':
                 return this.add(numbers)
@@ -11,6 +13,20 @@ class Calculator {
                 return this.divide(numbers)
             default:
                 return
+        }
+    }
+
+    getOperatorFromInput(input) {
+        if (input.includes('+')) {
+            return ['+', input.split('+').map(num => Number(num))]
+        } else if (input.includes('-')) {
+            return ['-', input.split('-').map(num => Number(num))]
+        } else if (input.includes('*')) {
+            return ['*', input.split('*').map(num => Number(num))]
+        } else if (input.includes('/')) {
+            return ['/', input.split('/').map(num => Number(num))]
+        } else {
+            return ''
         }
     }
 
@@ -29,4 +45,11 @@ class Calculator {
     divide(numbers) {
         return numbers.reduce((acc, current) => acc / current)
     }
+}
+
+function operate() {
+    const input = document.querySelector("input")
+    const calculator = new Calculator()
+
+    input.value = calculator.operate(input.value)
 }
